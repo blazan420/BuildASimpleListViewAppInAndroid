eb45e7a8eeaf486d5be68c6140d3852c3eb7e0d5
kevin <kevinjobrien420@gmail.com>
2018-06-27 03:26:09 -0500

    247 App Number 33 - Part1 - Build a Simple ListView App in Android // Coding

--- app/src/main/java/com/example/kevin/buildasimplelistviewappinandroid/MainActivity.java
+++ app/src/main/java/com/example/kevin/buildasimplelistviewappinandroid/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.kevin.buildasimplelistviewappinandroid;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener {
+
+    String[] courseNames = {"Android Developer Course", "iOS Developer Course", "Java Developer Course",
+            "Objective-C Developer Course", "Web Developer Course"};
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.activity_main, courseNames);
+
+        setListAdapter(adapter);
+
+
+        ListView lstView = getListView();
+
+        lstView.setOnItemClickListener(MainActivity.this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
